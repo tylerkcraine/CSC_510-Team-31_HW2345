d8d2671f285c0ae47e36b3b1d9f0c3b18e70abfe
Tyler Craine <tylerkecraine@gmail.com>
2022-09-05 20:00:03 -0400

    refactor __init__.py to add functionally, refactored fileutils.py to work with new args object

--- code/__init__.py
+++ code/__init__.py
@@ -1,3 +1,13 @@
-import sys
+import args
+import fileutils
+import print as custom_print
+from parse_function import parse
 
-print(sys.argv)
+def main():
+    the_args = args.The()
+    file_string = fileutils.read_file(the_args)
+    parser_dict = parse(file_string)
+    custom_print.o(parser_dict)
+
+if __name__ == '__main__':
+    main()
--- code/fileutils.py
+++ code/fileutils.py
@@ -1,15 +1,15 @@
-import fileparser
+import args
 
 
-def read_file(the):
-    file_name = the['f']
+def read_file(argus: args.The) -> str:
+    print("butts")
+    file_name = argus.the["f"]
     buffer_string = ""
     with open(file_name, "r") as csv_file:
         file_lines = csv_file.readlines()
     for i in file_lines:
         buffer_string = buffer_string + i
-    print(buffer_string)
-    fileparser.parse(buffer_string)
+    return buffer_string
 
 
 
