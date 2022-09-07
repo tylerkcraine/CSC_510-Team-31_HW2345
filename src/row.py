b0f5ca8551edd79c39109ee58208318037f995d3
ntgomes <ntgomes@ncsu.edu>
2022-09-07 08:40:10 -0400

    Implement custom deepcopy function for use in row

--- src/row.py
+++ src/row.py
@@ -1,8 +1,8 @@
-import copy 
+from utils import copy
 
 
 class Row:
   def __init__(self, t):
     self.cell = t
-    self.cooked = copy.deepcopy(t)
+    self.cooked = copy(t)
     self.isEvaled = False
